ae8cb50f0cb9b74abb8c01178a5d3f8e31b1efa7
Jindra Helcl <helcl@ufal.mff.cuni.cz>
2024-04-23 12:43:50 +0200

    more basic template

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/annotate/<username>")
+def annotate(username):
+    return render_template("annotate.html", username=username)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
